eab4ee9241b4060238c6cd5d8b7f652812d92641
Arman Safikhani <Arman1371@gmail.com>
2017-01-25 00:25:57 +0330

    Added expandablePanel

---
+++ js/common/ExpandablePanel.js
@@ -0,0 +1,104 @@
+import React,{Component,StyleSheet,View,Image,TouchableHighlight,Animated} from 'react-native';
+var { Text } = require('./FmText');
+
+class ExpandablePanel extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            title       : props.title,
+            expanded    : true,
+            animation   : new Animated.Value()
+        };
+    }
+
+    toggle(){
+        let initialValue    = this.state.expanded ? this.state.maxHeight + this.state.minHeight : this.state.minHeight,
+            finalValue      = this.state.expanded ? this.state.minHeight : this.state.maxHeight + this.state.minHeight;
+
+        this.setState({
+            expanded : !this.state.expanded
+        });
+
+        this.state.animation.setValue(initialValue);
+        Animated.spring(
+            this.state.animation,
+            {
+                toValue: finalValue
+            }
+        ).start();
+    }
+
+    _setMaxHeight(event){
+        this.setState({
+            maxHeight   : event.nativeEvent.layout.height
+        });
+    }
+
+    _setMinHeight(event){
+        this.setState({
+            minHeight   : event.nativeEvent.layout.height
+        });
+    }
+
+    render(){
+        let icon = this.icons['down'];
+
+        if(this.state.expanded){
+            icon = this.icons['up'];
+        }
+
+        return (
+            <Animated.View 
+                style={[styles.container,{height: this.state.animation}]}>
+                <View style={styles.titleContainer} onLayout={this._setMinHeight.bind(this)}>
+                    <Text style={styles.title}>{this.state.title}</Text>
+                    <TouchableHighlight 
+                        style={styles.button} 
+                        onPress={this.toggle.bind(this)}
+                        underlayColor="#f1f1f1">
+                        <Image
+                            style={styles.buttonImage}
+                            source={icon}
+                        ></Image>
+                    </TouchableHighlight>
+                </View>
+                
+                <View style={styles.body} onLayout={this._setMaxHeight.bind(this)}>
+                    {this.props.children}
+                </View>
+
+            </Animated.View>
+        );
+    }
+}
+
+var styles = StyleSheet.create({
+    container   : {
+        backgroundColor: '#fff',
+        margin:10,
+        overflow:'hidden'
+    },
+    titleContainer : {
+        flexDirection: 'row'
+    },
+    title       : {
+        flex    : 1,
+        padding : 10,
+        color   :'#2a2f43',
+        fontWeight:'bold'
+    },
+    button      : {
+
+    },
+    buttonImage : {
+        width   : 30,
+        height  : 25
+    },
+    body        : {
+        padding     : 10,
+        paddingTop  : 0
+    }
+});
+
+export default ExpandablePanel;
